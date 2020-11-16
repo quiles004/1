051696c4c6a14a0d6852aefa0c5b43a51016466d
quiles004 <73704174+quiles004@users.noreply.github.com>
2020-11-16 09:11:06 +0100

    Update consultas.js

--- consultas.js
+++ consultas.js
@@ -7,8 +7,11 @@ db.final.find({$and:[{tiros_libres:{$lt:"76"}},{altura:{$gte:200}}]}).pretty();
 /*jugadores que su posicion en el campo de sea base*/
 db.final.find({$and:[{posicion:{$ne:"base"}}]}).pretty();
 /*jugadores que su nacionalidad no empiece por E y que no tengan una altura menor o igual de 202 cms*/
-db.final.find({$and:[{nacionalidad:{$not:{$regex:/E/}},
-                      $nor:[{altura:{$lte:202}}]}]}).pretty();
+db.final.find
+({$and:[
+  {nacionalidad:{$not:{$regex:/E/}},
+      $nor:[{altura:{$lte:202}}]}]})
+  .pretty();
 
 /*{
         "_id" : ObjectId("5fb231c05b9e3f31c5331b46"),
@@ -63,10 +66,12 @@ db.final.find({$and:[{nacionalidad:{$not:{$regex:/E/}},
         "nacimiento" : ISODate("1996-08-19T22:00:00Z")
 }*/
 /*jugadores que su nacionalidad no empiece por la latra "e", que tengan un premio de mvp, que midan mas de 205 cm y su porcentaje de tiros de campo sea mayor del 43%*/
-db.final.find({$and:[{nacionalidad:{$not:{$regex:/E/}}},
-                     {mvp:true},
-                     {altura:{$gte:205}},
-                     {tiros_de_campo:{$gt:"43"}}]}).pretty();
+db.final.find({$and:[
+  {nacionalidad:{$not:{$regex:/E/}}},
+       {mvp:true},
+       {altura:{$gte:205}},
+       {tiros_de_campo:{$gt:"43"}}]})
+  .pretty();
 
 /*{
         "_id" : ObjectId("5fb231c05b9e3f31c5331b46"),
@@ -84,9 +89,11 @@ db.final.find({$and:[{nacionalidad:{$not:{$regex:/E/}}},
         "nacimiento" : ISODate("1995-01-05T23:00:00Z")
 }*/
 /*jugadores que jueguen de alero o ala-pivot, que tengan un premio de mvp, y que su porcentaje de tiros de campo sea igual o superior al 52%*/
-db.final.find({$and:[{posicion:{$all:["alero","ala-pivot"]}},
-                     {mvp:true},
-                     {tiros_de_campo:{$gte:"52"}}]}).pretty();
+db.final.find({$and:[
+  {posicion:{$all:["alero","ala-pivot"]}},
+   {mvp:true},
+   {tiros_de_campo:{$gte:"52"}}]})
+  .pretty();
 
 /*{
         "_id" : ObjectId("5fb231c05b9e3f31c5331b45"),
@@ -119,9 +126,12 @@ db.final.find({$and:[{posicion:{$all:["alero","ala-pivot"]}},
         "nacimiento" : ISODate("1995-01-05T23:00:00Z")
 }*/
 /*jugadores que jueguen de alero que no tengan un premio de mvp que nacieran antes del 1-1-1992 y ordenados por fecha*/
-db.final.find({$and:[{posicion:{$eq:"alero"}},
-                     {mvp:false},
-                     {nacimiento:{$lt: new Date(1992,1,1)}}]}).sort({nacimiento:1}).pretty();
+db.final.find({$and:[
+  {posicion:{$eq:"alero"}},
+  {mvp:false},
+  {nacimiento:{$lt: new Date(1992,1,1)}}]})
+  .sort({nacimiento:1})
+  .pretty();
 
 /*{
         "_id" : ObjectId("5fb231c05b9e3f31c5331b50"),
@@ -152,9 +162,12 @@ db.final.find({$and:[{posicion:{$eq:"alero"}},
         "nacimiento" : ISODate("1991-07-28T22:00:00Z")
 }*/
 /*jugadores que puedan jugar en 3 posiciones diferentes que tengan un porcentaje de tiros libres mayor a el 70% y que hayan nacido antes del 1-1-1990, ordenados por el porcentaje de tiros libres*/
-db.final.find({$and:[{posicion:{$size:3}},
-                     {tiros_libres:{$gte:"70"}},
-                     {nacimiento:{$lt: new Date(1990,1,1)}}]}).sort({tiros_libres:-1}).pretty();
+db.final.find({$and:[
+  {posicion:{$size:3}},
+  {tiros_libres:{$gte:"70"}},
+  {nacimiento:{$lt: new Date(1990,1,1)}}]})
+  .sort({tiros_libres:-1})
+  .pretty();
 
 /*{
         "_id" : ObjectId("5fb231c05b9e3f31c5331b45"),
